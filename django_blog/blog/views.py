838f8b70fa191c06106400f1e83750a311028952
LuosRestil <metaltasteslikechicken@gmail.com>
2020-07-21 15:18:54 -0500

    commit before adding user registration

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,30 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'August 28, 2018'
-    },
-    {
-        'author': 'Hil Phartman',
-        'title': 'Wiggle Worm',
-        'content': 'Spoke too soon, wiggle worm.',
-        'date_posted': 'August 29, 2018'
-    }
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
-    context = {'posts': posts}
+    context = {'posts': Post.objects.all()}
     return render(request, 'blog/home.html', context)
 
 def about(request):
