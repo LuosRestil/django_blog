c3b23b38d4dd4d1b55f3a0edc20824a0cee3feab
LuosRestil <metaltasteslikechicken@gmail.com>
2020-07-22 15:22:54 -0500

    Added detail, create, update, and delete class based views for posts, and list view for home page

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,71 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
-def home(request):
-    context = {'posts': Post.objects.all()}
-    return render(request, 'blog/home.html', context)
+
+# def home(request):
+#     context = {'posts': Post.objects.all()}
+#     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # default: <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 def about(request):
     context = {'title': 'About'}
     return render(request, 'blog/about.html', context)
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    success_message = "Post created."
+    success_url = reverse_lazy('blog-home')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    success_message = "Post updated."
+    success_url = reverse_lazy('blog-home')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, DeleteView):
+    model = Post
+
+    success_message = "Post deleted."
+    success_url = reverse_lazy('blog-home')
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+    def delete(self, request, *args, **kwargs):
+        obj = self.get_object()
+        data_to_return = super(PostDeleteView, self).delete(request, *args, **kwargs)
+        messages.success(self.request, self.success_message % obj.__dict__)
+        return data_to_return
