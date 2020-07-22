381663b86a97e1d410bc7ec5135425b2a35e70a0
LuosRestil <metaltasteslikechicken@gmail.com>
2020-07-22 10:35:41 -0500

    Completed user registration, login, logout

--- django_blog/users/views.py
+++ django_blog/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
 from django.contrib import messages
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -9,10 +11,18 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}.')
-            return redirect('blog-home')
+            messages.success(request, f'Account created for {username}. You are now able to log in.')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/register.html', context)
     
+def logout(request):
+    django_logout(request)
+    messages.success(request, f'Logged out.')
+    return redirect('login')
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
